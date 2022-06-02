e26ad14efe042272234af8f76db5345c6e970820
Sultonov Komron <k.sultonov@usoft.uz>
2022-06-02 14:44:03 +0500

    Update Project

--- src/locales/uz/index.js
+++ src/locales/uz/index.js
@@ -156,7 +156,7 @@ export default {
     debt: 'Qarz',
     go_to_contract: "Shartnomaga o'tish",
     week: 'Hafta',
-    today: 'Сегодня',
+    today: 'Bugun',
 
     tab_status: {
         all: "Hammasi",
