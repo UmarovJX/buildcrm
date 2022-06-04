a4f466c7f246283b1d706b1b54550bf37c258e6d
Sultonov Komron <k.sultonov@usoft.uz>
2022-06-04 18:00:29 +0500

    Calendar UI

---
+++ src/components/Debtors/BaseHugeCalendarUI.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+<!--    <span v-for="">-->
+
+<!--    </span>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseHugeCalendarUI",
+  props: {
+    month: {
+      type: String,
+      required: true
+    },
+    year: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
