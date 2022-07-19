db21f651b26fcbd188d0d8b17c55a2104c820fc3
Sultonov Komron <k.sultonov@usoft.uz>
2022-07-19 16:04:41 +0500

    Add Plans Permission

---
+++ src/permission/plans.js
@@ -0,0 +1,36 @@
+import Permission from "@/permission/index";
+
+export default class Promos extends Permission {
+    static plans() {
+        return super.getUserPermission('plans')
+    }
+
+    static getPromosPermission(property) {
+        const splitProperty = property.split('.')
+        const [one, two] = splitProperty
+        if (splitProperty.length > 1) {
+            if (this.hasAdminRole()) return true
+            return this.plans()[one][two] ?? false
+        } else {
+            if (this.hasAdminRole()) return true
+            return this.plans()[one] ?? false
+        }
+
+    }
+
+    static getPlansCreatePermission(){
+        return this.getPromosPermission('create')
+    }
+
+    static getPlansViewPermission() {
+        return this.getPromosPermission('view')
+    }
+
+    static getPlansEditPermission(){
+        return this.getPromosPermission('edit')
+    }
+
+    static getPlansDeletePermission(){
+        return this.getPromosPermission('delete')
+    }
+}
