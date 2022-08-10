f16564a0ef57f1232aa99e3abe6a12a839506829
Sultonov Komron <k.sultonov@usoft.uz>
2022-08-10 15:57:30 +0500

    Add New Functionality To Be Able To Generate Last Day of Month When Choose Last Day Of Month To The Monthly Payment In The Checkout Page

--- src/util/calculator.js
+++ src/util/calculator.js
@@ -187,33 +187,38 @@ export function getTotalDiscount(apartments, contract) {
 }
 
 
+import {dateProperties} from "@/util/calendar";
+
 export function CreditMonths(apartments, contract) {
-    let today = contract.payment_date ? new Date(contract.payment_date) : new Date();
-    contract.credit_months = [];
+    let today = contract.payment_date ? new Date(contract.payment_date) : new Date()
+    const {year: todayYear, month: todayMonth} = dateProperties(today)
+    let calculateByLastDay = today.getDate() === (new Date(todayYear, todayMonth + 1, 0)).getDate()
+    contract.credit_months = []
 
     if (parseInt(contract.month) > 0) {
-        let month_amount = getMonth(apartments, contract);
-
+        let month_amount = getMonth(apartments, contract)
         for (let i = 0; i < parseInt(contract.month); i++) {
             if (i === 0) {
+                const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0)
                 contract.credit_months.push({
-                    month: today.setMonth(today.getMonth()),
+                    month: calculateByLastDay ? lastDayOfMonth : today.setMonth(today.getMonth()),
                     amount: month_amount,
                     edit: false,
                     edited: false,
-                });
+                })
             } else {
+                const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1 + i, 0)
                 contract.credit_months.push({
-                    month: today.setMonth(today.getMonth() + 1),
+                    month: calculateByLastDay ? lastDayOfMonth : today.setMonth(today.getMonth() + 1),
                     amount: month_amount,
                     edit: false,
                     edited: false,
-                });
+                })
             }
         }
     }
 
-    return contract;
+    return contract
 }
 
 export function editedCreditMonths(apartments, contract) {
