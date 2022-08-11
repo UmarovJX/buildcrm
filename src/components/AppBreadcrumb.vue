5a56eeb89060d340c5851a0aa190a35418655d22
Sultonov Komron <k.sultonov@usoft.uz>
2022-08-11 17:37:33 +0500

    Create Global Breadcrumb Belongs To New Design

---
+++ src/components/AppBreadcrumb.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="app-breadcrumb d-flex align-items-center">
+    <div v-for="(breadcrumb,index) in breadcrumbs" :key="index" :class="breadcrumb.class">
+      <router-link :to="{name:breadcrumb.route.name}" :class="breadcrumb.route.class" class="app-breadcrumb-link">
+        <slot :name="`${breadcrumb.route.name}-left`"></slot>
+        <span class="app-breadcrumb-link-content">
+          {{ (breadcrumb.content.type === 'string') ? breadcrumb.content.path : $t(breadcrumb.content.path) }}
+        </span>
+        <slot :name="`${breadcrumb.route.name}-right`"></slot>
+        <span>
+          <base-right-icon :width="18" :height="18"/>
+        </span>
+      </router-link>
+    </div>
+    <span class="app-breadcrumb-page">
+      {{ (page.type === 'string') ? page.path : $t(page.path) }}
+    </span>
+  </div>
+</template>
+
+<script>
+import BaseRightIcon from "@/components/icons/BaseRightIcon";
+
+export default {
+  name: "AppBreadcrumb",
+  components: {
+    BaseRightIcon
+  },
+  props: {
+    page: {
+      type: Object,
+      required: true
+      /*
+      *
+      * Structure
+      * {
+      *   type:'',
+      *   path:''
+      * }
+      *
+      * */
+    },
+    breadcrumbs: {
+      type: Array,
+      required: true
+      /*
+      *
+      * Structure
+      * [
+      *   {
+      *     content:{
+      *       type:'string',
+      *       path:''
+      *     },
+      *     route:{
+      *       name:'',
+      *       path:'',
+      *       params:'',
+      *       query:'',
+      *       class:'',
+      *     }
+      *     class:''
+      *   }
+      * ]
+      *
+      * */
+    }
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+.app-breadcrumb {
+  color: var(--gray-400);
+  font-size: 14px;
+  font-weight: 600;
+  font-family: Inter, sans-serif;
+
+  &-link {
+
+    &:hover {
+      color: var(--violet-600);
+    }
+
+    &-content:not(:first-child) {
+      padding-right: 10px;
+      padding-left: 10px;
+    }
+  }
+
+  &-page {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+}
+</style>
