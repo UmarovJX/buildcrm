ee310b30ee7d7da444aafd62d1590360d181197e
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-08-17 11:37:03 +0500

    topbar

--- src/views/Documentation/Topbar.vue
+++ src/views/Documentation/Topbar.vue
@@ -16,6 +16,32 @@
         </template>
       </BaseButton>
     </div>
+    <div class="d-flex">
+      <BaseAvatar class="mr-4">
+        <template #avatar>
+          <img src="@/assets/img/avatar.svg" alt="logo"/>
+        </template>
+        <template #full_name>
+          Достонхон Озодходжаев
+        </template>
+        <template #role>
+          {{ $t("roles.admin") }}
+        </template>
+      </BaseAvatar>
+      <BaseAvatar background="#E5E7EB" avatar="avatar.svg" full_name="Достонхон Озодходжаев" :role="$t('roles.admin')"/>
+    </div>
+    <div class="just-avatar">
+      <BaseAvatar avatar="avatar.svg" class="mr-4"/>
+      <BaseAvatar background="#E5E7EB">
+        <template #avatar>
+          <img src="@/assets/img/avatar.svg" alt="logo"/>
+        </template>
+      </BaseAvatar>
+    </div>
+    <div class="timer">
+      <BaseTimer minute="29" second="59" background="linear-gradient(88.25deg, #7C3AED 0%, #818CF8 100%)"/>
+      <BaseTimer minute="29" second="59"/>
+    </div>
   </div>
 </template>
 
@@ -23,9 +49,11 @@
 
 import BaseButton from "@/components/Reusable/BaseButton";
 import BaseNotificationIcon from "@/components/icons/BaseNotificationsIcon";
+import BaseAvatar from "@/components/Reusable/BaseAvatar";
+import BaseTimer from "@/components/Reusable/BaseTimer";
 export default {
   name: 'Topbar',
-  components: {BaseNotificationIcon, BaseButton},
+  components: {BaseTimer, BaseAvatar, BaseNotificationIcon, BaseButton},
 }
 </script>
 
@@ -57,8 +85,29 @@ export default {
       line-height: 77px;
       color: #4B5563;
     }
-    .base__button {
-      margin-left: 0;
+  }
+::v-deep .base__button {
+  margin-left: 0;
+  .right__icon {
+    margin-left: 0!important;
+  }
+}
+::v-deep .base-avatar:last-child {
+  .person {
+    .full_name {
+      color: #7C3AED;
     }
   }
+}
+.just-avatar {
+  display: flex;
+  .base-avatar {
+    gap: 0;
+    padding-right: 8px;
+  }
+}
+.timer {
+  display: flex;
+  gap: 16px;
+}
 </style>
