0bec4c6bde71d98aeb79b437d4504eb152fae5a5
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-08-18 11:55:08 +0500

    added buttons doc

--- src/views/Documentation/Buttons.vue
+++ src/views/Documentation/Buttons.vue
@@ -67,7 +67,7 @@
             </template>
           </BaseButton>
           <BaseButton text="">
-            <template #left-icon>
+            <template #right-icon>
               <BaseLoading :width="24" :height="24"/>
             </template>
           </BaseButton>
@@ -77,7 +77,7 @@
           <BaseMultiselect track-by="value" label="text" :options="options"/>
           <BaseMultiselect track-by="value" label="text" :options="options"/>
           <BaseButton text="">
-            <template #left-icon>
+            <template #right-icon>
               <BaseLoading :width="24" :height="24"/>
             </template>
           </BaseButton>
@@ -87,11 +87,29 @@
           <BaseButton text="Текст кнопки"/>
           <BaseButton text="Текст кнопки"/>
           <BaseButton text="">
-            <template #left-icon>
+            <template #right-icon>
               <BaseLoading :width="24" :height="24"/>
             </template>
           </BaseButton>
         </div>
+      </div>
+      <div class="button-content-navigation">
+        <div class="navigation">
+          <BaseLeftIcon/>
+        </div>
+        <div class="navigation">
+          <BaseLeftIcon disabled="true"/>
+        </div>
+      </div>
+      <div class="button-content-pure-button">
+        <BaseButton text="Текст кнопки"/>
+        <BaseButton text="Текст кнопки"/>
+        <BaseButton text="Текст кнопки"/>
+        <BaseButton text="">
+          <template #left-icon>
+            <BaseLoading :width="24" :height="24"/>
+          </template>
+        </BaseButton>
       </div>
     </div>
   </div>
@@ -102,9 +120,10 @@ import BaseButton from "@/components/Reusable/BaseButton";
 import BaseFilterIcon from "@/components/icons/BaseFilterIcon";
 import BaseLoading from "@/components/Reusable/BaseLoading";
 import BaseMultiselect from "@/components/Reusable/BaseMultiselect";
+import BaseLeftIcon from "@/components/icons/BaseLeftIcon";
 export default {
   name: "Buttons",
-  components: {BaseMultiselect, BaseLoading, BaseFilterIcon, BaseButton},
+  components: {BaseLeftIcon, BaseMultiselect, BaseLoading, BaseFilterIcon, BaseButton},
   data() {
     return {
       options: [
@@ -173,6 +192,45 @@ export default {
         min-width: 380px;
       }
     }
+    &-navigation {
+      display: flex;
+      margin-bottom: 7rem;
+      gap: 64px;
+      .navigation {
+        padding: 12px;
+        background: #F3F4F6;
+        border-radius: 50%;
+        svg {
+          width: 32px;
+          height: 32px;
+        }
+        &:last-child {
+          background-color: #E5E7EB;
+        }
+      }
+    }
+    &-pure-button {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      .base__button:nth-child(2n+1) {
+        background-color: transparent;
+      }
+      .base__button:nth-child(2) {
+        background: #E5E7EB;
+      }
+      ::v-deep.base__button:nth-child(3) {
+        .text {
+          color: #9CA3AF;
+        }
+      }
+      ::v-deep.base__button:last-child {
+        padding: 16px 65px;
+        .left__icon {
+            margin-right: 0;
+        }
+      }
+    }
   }
   .search-content-item:nth-child(2){
     ::v-deep .search__content-input {
@@ -194,13 +252,17 @@ export default {
       flex-wrap: wrap;
       flex-direction: column;
       gap: 30px;
-      margin-bottom: 5rem;
+      margin-bottom: 7rem;
       &-row {
         display: flex;
         gap: 64px;
+        ::v-deep.base__button {
+          padding: 17px 24px;
+        }
         ::v-deep .base__multiselect:nth-child(2) {
           .multiselect__tags {
             background-color: #E5E7EB;
+            padding: 17px 24px;
           }
         }
         ::v-deep .base__multiselect:nth-child(3) {
@@ -223,18 +285,48 @@ export default {
           }
         }
       }
+      &-row:last-child {
+        ::v-deep.base__button:nth-child(4) {
+          padding: 16px 70px 16px 60px!important;
+          .left__icon {
+            width: 10px;
+          }
+        }
+      }
     }
     &-part2 {
       display: flex;
       flex-wrap: wrap;
       flex-direction: column;
       gap: 30px;
+      margin-bottom: 7rem;
+
       ::v-deep .base__multiselect {
+        .multiselect__select {
+          filter: brightness(0) invert(1);
+        }
         .multiselect__tags {
           background: linear-gradient(88.25deg, #7C3AED 0%, #818CF8 100%);
+          padding: 17px 24px;
         }
         .multiselect__placeholder {
           color: white;
+        }
+      }
+      ::v-deep .base__multiselect:nth-child(2) {
+        .multiselect__tags {
+          background: #6D28D9;
+        }
+      }
+      ::v-deep .base__multiselect:nth-child(3) {
+        .multiselect__select {
+          filter: brightness(1) invert(0);
+        }
+        .multiselect__tags {
+          background: #F3F4F6;
+        }
+        .multiselect__placeholder {
+          color: #9CA3AF;
         }
       }
       &-row {
@@ -245,18 +337,60 @@ export default {
         }
         ::v-deep.base__button {
           background: linear-gradient(88.25deg, #7C3AED 0%, #818CF8 100%);
+          padding: 17px 24px;
+          .left__icon {
+            svg {
+              path {
+                fill: white;
+              }
+            }
+          }
           .text{
             color: white;
           }
         }
         ::v-deep.base__button:nth-child(2) {
-          background-color: #6D28D9;
-          .text{
-            color: white;
+          background: #6D28D9;
+        }
+        ::v-deep.base__button:nth-child(3) {
+          background: #F3F4F6;
+          .text {
+            color: #9CA3AF;
           }
+          .left__icon {
+            svg {
+              path {
+                fill: #9CA3AF;
+              }
+            }
+          }
+        }
+        ::v-deep.base__button:nth-child(4) {
+          .right__icon {
+            svg {
+              path:first-child {
+                fill: #6D28D9;
+              }
+              path:last-child {
+                fill: #FFFFFF;
+              }
+            }
+          }
+        }
+      }
+      &-row:last-child {
+        ::v-deep.base__button:last-child {
+          padding: 16px 61px 16px 51px;
+        }
+      }
+      .base__button:last-child {
+        padding: 16px 85px 16px 65px;
+        ::v-deep .right__icon {
+          margin-right: 0;
         }
       }
     }
     }
-}
+  }
+
 </style>
