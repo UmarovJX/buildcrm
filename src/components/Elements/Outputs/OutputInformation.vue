71644cdd3d77883a280e6c2498ff54daa5468642
Maftuna Vohidjonovna <maisiedev@gmail.com>
2022-08-24 10:39:41 +0500

    added text fields

--- src/components/Elements/Outputs/OutputInformation.vue
+++ src/components/Elements/Outputs/OutputInformation.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="output-information d-flex justify-content-between align-items-center">
     <!--  PROPERTY CONTENT  -->
-    <slot name="property"/>
-    <span
+    <div class="d-flex align-items-center info-row">
+      <slot name="left-icon"/>
+      <slot name="property"/>
+      <span
         v-if="!hasPropertySlot"
         class="d-block property"
     >
-      {{ localProperty }}
-    </span>
+        {{ localProperty }}
+      </span>
+    </div>
     <!--  END OF PROPERTY CONTENT  -->
 
     <!--  VALUE CONTENT  -->
-    <slot name="value"/>
-    <span
-        v-if="!hasValueSlot"
-        class="d-block value"
-    >
+    <div class="d-flex align-items-center ">
+      <slot name="value"/>
+      <span
+          v-if="!hasValueSlot"
+          class="d-flex value align-items-center info-row"
+      >
       <span>{{ value }}</span>
       <span v-if="price"> {{ $t('ye') }}</span>
+      <slot class="info-row-icon" name="right-icon"/>
     </span>
+    </div>
     <!--  END OF VALUE CONTENT  -->
   </div>
 </template>
@@ -74,7 +80,7 @@ export default {
 
 <style lang="scss" scoped>
 .output-information {
-  border: 2px solid var(--gray-200);
+  border: 3px solid var(--gray-200);
   border-radius: 2rem;
   background-color: white;
   padding: 1rem 1.75rem;
@@ -92,6 +98,21 @@ export default {
     font-size: 18px;
     color: var(--gray-600) !important;
     line-height: 22px;
+  }
+  .info-row {
+    gap: 16px;
+    &-icon {
+      background: #EDE9FE;
+      border-radius: 50%;
+      padding: 0 5px 3px;
+    }
+    &-icon:nth-child(2) {
+      background: #7C3AED;
+      padding: 2px 5px 5px;
+    }
+  }
+  .info-row:first-child {
+    gap: 12px;
   }
 }
 </style>
