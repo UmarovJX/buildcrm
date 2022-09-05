62129401aad565eb54bc61b3a09c4e1fe2b04b8a
gafurov8020 <67802374+gafurov8020@users.noreply.github.com>
2022-09-05 11:54:51 +0500

    roles bugs fixed

--- src/components/Dashboard/Roles/Index.vue
+++ src/components/Dashboard/Roles/Index.vue
@@ -288,123 +288,123 @@
 
 <!--</style>-->
 <template>
-  <main class="main__content">
-    <div class="app-content">
-      <div
-          class="
+    <main class="main__content">
+        <div class="app-content">
+            <div
+                class="
           d-flex
-          justify-content-between
+          justify-content-end
           align-items-center
           flex-md-row flex-column
         "
-      >
-        <div class="d-flex align-items-center">
-          <base-search-input class="w-100" :placeholder="$t('users.placeholder')"/>
-          <base-button
-              v-if="getPermission.users && getPermission.users.create"
-              design="violet-gradient"
-              :text="$t('add')"
-              v-b-modal.modal-create
-              class="ml-4"
-          >
-            <template #left-icon>
-              <i class="fal fa-plus mr-2"></i>
-            </template>
-          </base-button>
-        </div>
+            >
+                <!--                <div class="d-flex align-items-center">-->
+                <!--                    <BaseSearchInput class="w-100" :placeholder="$t('users.placeholder')"/>-->
+                <!--                    <base-button-->
+                <!--                        v-if="getPermission.users && getPermission.users.create"-->
+                <!--                        design="violet-gradient"-->
+                <!--                        :text="$t('add')"-->
+                <!--                        v-b-modal.modal-create-->
+                <!--                        class="ml-4"-->
+                <!--                    >-->
+                <!--                        <template #left-icon>-->
+                <!--                            <i class="fal fa-plus mr-2"></i>-->
+                <!--                        </template>-->
+                <!--                    </base-button>-->
+                <!--                </div>div-->
 
-        <base-button
-            v-if="createPermission"
-            design="violet-gradient"
-            @click="$router.push({name: 'roles-store'})"
-            :text="$t('add')"
-            class="ml-4"
-        >
-          <template #left-icon>
-            <i class="fal fa-plus mr-2"></i>
-          </template>
-        </base-button>
 
-      </div>
+                <base-button
+                    v-if="createPermission"
+                    design="violet-gradient"
+                    @click="$router.push({name: 'roles-store'})"
+                    :text="$t('add')"
+                    class="ml-4"
+                >
+                    <template #left-icon>
+                        <BasePlusIcon fill="var(--white)"/>
+                    </template>
+                </base-button>
+            </div>
 
-      <div class="">
-        <b-table
-            sticky-header
-            borderless
-            responsive
-            :items="getRoles"
-            :fields="fields"
-            :busy="getLoading"
-            show-empty
-            :sort-by.sync="sortBy"
-            :sort-desc.sync="sortDesc"
-            sort-icon-left
-            class="custom-table"
-            :empty-text="$t('no_data')"
-        >
-          <template #empty="scope" class="text-center">
+            <div class="">
+                <b-table
+                    sticky-header
+                    borderless
+                    responsive
+                    :items="getRoles"
+                    :fields="fields"
+                    :busy="getLoading"
+                    show-empty
+                    :sort-by.sync="sortBy"
+                    :sort-desc.sync="sortDesc"
+                    sort-icon-left
+                    class="custom-table"
+                    :empty-text="$t('no_data')"
+                >
+                    <template #empty="scope" class="text-center">
             <span class="d-flex justify-content-center align-items-center">
               {{ scope.emptyText }}</span>
-          </template>
+                    </template>
 
-          <template #table-busy>
-            <div class="d-flex justify-content-center w-100">
-              <div class="lds-ellipsis">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-              </div>
+                    <template #table-busy>
+                        <div class="d-flex justify-content-center w-100">
+                            <div class="lds-ellipsis">
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </div>
+                    </template>
+
+                    <template #cell(name)="data">
+                        {{ getName(data.item.name) }}
+                    </template>
+
+                    <template #cell(actions)="data">
+                        <div class="float-right">
+                            <div
+                                class="dropdown my-dropdown dropleft"
+                                v-if="data.item.id !== 1 && (editPermission || deletePermission)"
+                            >
+                                <button
+                                    type="button"
+                                    class="dropdown-toggle"
+                                    data-toggle="dropdown"
+                                >
+                                    <i class="far fa-ellipsis-h"></i>
+                                </button>
+
+                                <div
+                                    class="dropdown-menu"
+                                    v-if="editPermission || deletePermission"
+                                >
+                                    <router-link
+                                        :to="{name: 'roles-update', params: {id: data.item.id}}"
+                                        v-if="data.item.id !== 1 && editPermission"
+                                        :class="'dropdown-item dropdown-item--inside'"
+                                    >
+                                        <i class="fas fa-pen"></i>
+                                        {{ $t("edit") }}
+                                    </router-link>
+
+                                    <a
+                                        class="dropdown-item dropdown-item--inside"
+                                        v-if="data.item.id !== 1 &&  deletePermission"
+                                        @click="deleteRole(data.item.id)"
+                                        href="#"
+                                    >
+                                        <i class="far fa-trash"></i> {{ $t("delete") }}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </template>
+                </b-table>
             </div>
-          </template>
-
-          <template #cell(name)="data">
-            {{ getName(data.item.name) }}
-          </template>
-
-          <template #cell(actions)="data">
-            <div class="float-right">
-              <div
-                  class="dropdown my-dropdown dropleft"
-                  v-if="data.item.id !== 1 && (editPermission || deletePermission)"
-              >
-                <button
-                    type="button"
-                    class="dropdown-toggle"
-                    data-toggle="dropdown"
-                >
-                  <i class="far fa-ellipsis-h"></i>
-                </button>
-
-                <div
-                    class="dropdown-menu"
-                    v-if="editPermission || deletePermission"
-                >
-                  <router-link
-                      :to="{name: 'roles-update', params: {id: data.item.id}}"
-                      v-if="data.item.id !== 1 && editPermission"
-                      :class="'dropdown-item dropdown-item--inside'"
-                  >
-                    <i class="fas fa-pen"></i>
-                    {{ $t("edit") }}
-                  </router-link>
-
-                  <a
-                      class="dropdown-item dropdown-item--inside"
-                      v-if="data.item.id !== 1 &&  deletePermission"
-                      @click="deleteRole(data.item.id)"
-                      href="#"
-                  >
-                    <i class="far fa-trash"></i> {{ $t("delete") }}
-                  </a>
-                </div>
-              </div>
-            </div>
-          </template>
-        </b-table>
-      </div>
-    </div>
-  </main>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -412,114 +412,118 @@ import {mapActions, mapGetters} from "vuex";
 import api from "@/services/api";
 import BaseButton from "@/components/Reusable/BaseButton";
 import RolesPermission from "@/permission/roles";
+import BasePlusIcon from "@/components/icons/BasePlusIcon";
+// import BaseSearchInput from "@/components/Reusable/BaseSearchInput";
 
 export default {
-  name: 'Roles',
-  components: {
-    BaseButton
-  },
-  data() {
-    return {
-      editPermission: RolesPermission.getRolesEditPermission(),
-      deletePermission: RolesPermission.getRolesDeletePermission(),
-      createPermission: RolesPermission.getRolesCreatePermission(),
-      header: {
-        headers: {
-          Authorization: "Bearer " + localStorage.token,
-        },
-      },
-      sortBy: "id",
-      sortDesc: false,
-      fields: [
-        {
-          key: "id",
-          label: "#",
-        },
-        {
-          key: "name",
-          label: this.$t("roles.name"),
-        },
-        {
-          key: "users_count",
-          label: this.$t("roles.users"),
-        },
-        {
-          key: "actions",
-          label: "",
-        },
-      ],
-    };
-  },
-
-  computed: mapGetters(["getRoles", "getPermission", "getLoading", "getMe"]),
-
-  mounted() {
-    this.fetchRoles(this);
-  },
-
-  methods: {
-    ...mapActions(["fetchRoles"]),
-
-    getName(name) {
-      let locale = localStorage.locale;
-      let value = "";
-
-      if (locale) {
-        switch (locale) {
-          case "ru":
-            value = name.ru;
-            break;
-          case "uz":
-            value = name.uz;
-            break;
-        }
-      } else {
-        value = name.ru;
-      }
-
-      return value;
+    name: 'Roles',
+    components: {
+        BasePlusIcon,
+        BaseButton,
+        // BaseSearchInput
+    },
+    data() {
+        return {
+            editPermission: RolesPermission.getRolesEditPermission(),
+            deletePermission: RolesPermission.getRolesDeletePermission(),
+            createPermission: RolesPermission.getRolesCreatePermission(),
+            header: {
+                headers: {
+                    Authorization: "Bearer " + localStorage.token,
+                },
+            },
+            sortBy: "id",
+            sortDesc: false,
+            fields: [
+                {
+                    key: "id",
+                    label: "#",
+                },
+                {
+                    key: "name",
+                    label: this.$t("roles.name"),
+                },
+                {
+                    key: "users_count",
+                    label: this.$t("roles.users"),
+                },
+                {
+                    key: "actions",
+                    label: "",
+                },
+            ],
+        };
     },
 
-    deleteRole(id) {
-      this.$swal({
-        title: this.$t("sweetAlert.title"),
-        text: this.$t("sweetAlert.are_you_sure_delete_role"),
-        icon: "warning",
-        showCancelButton: true,
-        cancelButtonText: this.$t("cancel"),
-        confirmButtonText: this.$t("sweetAlert.yes"),
-      }).then((result) => {
-        if (result.value) {
-          api.roles.deleteRole(id)
-              .then((response) => {
-                this.toasted(response.data.message, "success");
+    computed: mapGetters(["getRoles", "getPermission", "getLoading", "getMe"]),
 
-                this.fetchRoles(this);
+    mounted() {
+        this.fetchRoles(this);
+    },
 
-                this.$swal(this.$t("sweetAlert.deleted"), "", "success");
-              })
-              .catch((error) => {
-                if (!error.response) {
-                  this.toasted("Error: Network Error", "error");
-                } else {
-                  if (error.response.status === 403) {
-                    this.toasted(error.response.data.message, "error");
-                  } else if (error.response.status === 401) {
-                    this.toasted(error.response.data.message, "error");
-                  } else if (error.response.status === 500) {
-                    this.toasted(error.response.data.message, "error");
-                  } else if (error.response.status === 404) {
-                    this.toasted(error.response.data.exception, "error");
-                  } else {
-                    this.error = true;
-                    this.errors = error.response.data.errors;
-                  }
+    methods: {
+        ...mapActions(["fetchRoles"]),
+
+        getName(name) {
+            let locale = localStorage.locale;
+            let value = "";
+
+            if (locale) {
+                switch (locale) {
+                    case "ru":
+                        value = name.ru;
+                        break;
+                    case "uz":
+                        value = name.uz;
+                        break;
                 }
-              });
-        }
-      });
+            } else {
+                value = name.ru;
+            }
+
+            return value;
+        },
+
+        deleteRole(id) {
+            this.$swal({
+                title: this.$t("sweetAlert.title"),
+                text: this.$t("sweetAlert.are_you_sure_delete_role"),
+                icon: "warning",
+                showCancelButton: true,
+                cancelButtonText: this.$t("cancel"),
+                confirmButtonText: this.$t("sweetAlert.yes"),
+            }).then((result) => {
+                if (result.value) {
+                    api.roles.deleteRole(id)
+                        .then((response) => {
+                            this.toasted(response.data.message, "success");
+
+                            this.fetchRoles(this);
+
+                            this.$swal(this.$t("sweetAlert.deleted"), "", "success");
+                        })
+                        .catch((error) => {
+                            if (!error.response) {
+                                this.toasted("Error: Network Error", "error");
+                            } else {
+                                if (error.response.status === 403) {
+                                    this.toasted(error.response.data.message, "error");
+                                } else if (error.response.status === 401) {
+                                    this.toasted(error.response.data.message, "error");
+                                } else if (error.response.status === 500) {
+                                    this.toasted(error.response.data.message, "error");
+                                } else if (error.response.status === 404) {
+                                    this.toasted(error.response.data.exception, "error");
+                                } else {
+                                    this.error = true;
+                                    this.errors = error.response.data.errors;
+                                }
+                            }
+                        });
+                }
+            });
+        },
     },
-  },
 };
 </script>
 
