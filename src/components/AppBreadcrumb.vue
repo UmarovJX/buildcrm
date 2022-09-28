cdd52329f99957d1e1d4e1bf37a357f5bd16df8d
Komronbek17 <sultonovkomronbek17@gmail.com>
2022-09-27 18:26:40 -0700

    Add breadcrumb depend on new design

--- src/components/AppBreadcrumb.vue
+++ src/components/AppBreadcrumb.vue
@@ -1,38 +1,55 @@
 <template>
-  <div class="app-breadcrumb d-flex align-items-center">
-    <div v-for="(breadcrumb,index) in breadcrumbs" :key="index" :class="breadcrumb.class">
-      <router-link
-          :to="{
-            name:breadcrumb.route.name,
-            params:breadcrumb.route.params,
-            query:breadcrumb.route.query
-          }"
-          :class="breadcrumb.route.class"
-          class="app-breadcrumb-link"
-      >
-        <slot :name="`${breadcrumb.route.name}-left`"></slot>
-        <span class="app-breadcrumb-link-content">
-          {{ (breadcrumb.content.type === 'string') ? breadcrumb.content.path : $t(breadcrumb.content.path) }}
-        </span>
-        <slot :name="`${breadcrumb.route.name}-right`"></slot>
-        <span>
-          <base-right-icon :width="18" :height="18"/>
-        </span>
-      </router-link>
+  <div class="d-flex">
+    <div @click="$router.go(-1)" class="app-header-back-button d-flex justify-content-center align-items-center">
+      <base-left-icon :width="32" :height="32"/>
     </div>
-    <span class="app-breadcrumb-page">
-      {{ (page.type === 'string') ? page.path : $t(page.path) }}
-    </span>
+    <div class="app-header-page-content">
+      <div class="app-breadcrumb d-flex align-items-center">
+        <div v-for="(breadcrumb,index) in breadcrumbs" :key="index" :class="breadcrumb.class">
+          <router-link
+              :to="{
+                name:breadcrumb.route.name,
+                params:breadcrumb.route.params,
+                query:breadcrumb.route.query
+              }"
+              :class="breadcrumb.route.class"
+              class="app-breadcrumb-link"
+          >
+            <slot :name="`${breadcrumb.route.name}-left`"></slot>
+            <span class="app-breadcrumb-link-content">
+              {{ (breadcrumb.content.type === 'string') ? breadcrumb.content.path : $t(breadcrumb.content.path) }}
+            </span>
+            <slot :name="`${breadcrumb.route.name}-right`"></slot>
+            <span>
+              <base-right-icon :width="18" :height="18"/>
+            </span>
+          </router-link>
+        </div>
+        <span class="app-breadcrumb-page">
+          {{ (page.type === 'string') ? page.path : $t(page.path) }}
+        </span>
+      </div>
+      <div class="app-header-page-title">
+        <p>{{ pageInfo.title }}</p>
+        <p class="app-header-page-title-active">
+          {{ pageInfo.titleHighlight }}
+        </p>
+      </div>
+    </div>
+    <!-- TODO:SLOT BREADCRUMB_RIGHT   -->
+    <slot name="breadcrumb-right" />
   </div>
 </template>
 
 <script>
+import BaseLeftIcon from "@/components/icons/BaseLeftIcon";
 import BaseRightIcon from "@/components/icons/BaseRightIcon";
 
 export default {
   name: "AppBreadcrumb",
   components: {
-    BaseRightIcon
+    BaseRightIcon,
+    BaseLeftIcon
   },
   props: {
     page: {
@@ -72,7 +89,11 @@ export default {
       * ]
       *
       * */
-    }
+    },
+    pageInfo: {
+      type: Object,
+      required: true
+    },
   },
 
 }
@@ -114,6 +135,36 @@ export default {
   &-page {
     //padding-left: 10px;
     //padding-right: 10px;
+  }
+}
+
+.app-header {
+  font-family: Inter, sans-serif;
+
+  &-back-button {
+    padding: 12px;
+    cursor: pointer;
+    border-radius: 50%;
+    background-color: var(--gray-100);
+
+    &:hover {
+      background-color: var(--gray-200);
+    }
+  }
+
+  &-page-content {
+    margin-left: 1rem;
+  }
+
+  &-page-title {
+    font-size: 24px;
+    font-weight: 900;
+    color: var(--gray-600);
+    font-family: CraftworkSans, serif;
+
+    &-active {
+      color: var(--violet-600);
+    }
   }
 }
 </style>
