f67ea9c3d3ebe6978e7595785803bc067d113866
Komronbek17 <sultonovkomronbek17@gmail.com>
2022-10-18 15:21:52 +0500

    creating checkout starter

--- src/views/Experiment.vue
+++ src/views/Experiment.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <k-icon class="violet-600">
-      search
-    </k-icon>
-    <k-icon name="home" class="blue-600">
 
-    </k-icon>
   </div>
 </template>
 
 <script>
-import {KIcon} from "@/components/ui-components/material-icons";
 
 export default {
   name: 'Checkout',
   components: {
-    KIcon
   }
 }
 </script>
