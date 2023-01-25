dd87f630b423fac462c50ec916205d7563b153ce
gafurov8020 <67802374+gafurov8020@users.noreply.github.com>
2023-01-25 17:14:19 +0500

    contract date filter updated

--- src/locales/ru/index.js
+++ src/locales/ru/index.js
@@ -586,7 +586,7 @@ export default {
         object_name: 'Название объекта',
         apply_filter: 'Применить',
         client_type: 'Тип клиента',
-        date_type: 'Тип клиента',
+        date_type: 'Тип дата',
         reset_filter: 'Сбросить',
         agreement_date: 'Дата договора',
         apartment_number: 'Номер квартиры',
