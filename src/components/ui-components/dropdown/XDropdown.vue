7b524ffb533f4948c348414f4084c3d8613cb4c6
umarovjx <umjahongir@gmail.com>
2024-05-16 18:21:25 +0500

    upd

--- src/components/ui-components/dropdown/XDropdown.vue
+++ src/components/ui-components/dropdown/XDropdown.vue
@@ -1,24 +1,18 @@
 <script>
-import { XIcon } from '@/components/ui-components/material-icons'
+import { XIcon } from "@/components/ui-components/material-icons";
 
 export default {
   components: {
     XIcon,
   },
-}
+};
 </script>
 
 <template>
-  <b-dropdown
-    no-caret
-    class="b__dropdown__group"
-  >
+  <b-dropdown right no-caret class="b__dropdown__group">
     <template #button-content>
       <slot name="button-content" />
-      <x-icon
-        name="more_horiz"
-        class="violet-600"
-      />
+      <x-icon name="more_horiz" class="violet-600" />
     </template>
 
     <slot />
@@ -26,8 +20,8 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.b__dropdown__group{
-  ::v-deep .btn.dropdown-toggle{
+.b__dropdown__group {
+  ::v-deep .btn.dropdown-toggle {
     background-color: transparent !important;
     border: none !important;
     box-shadow: 0.5px 1px 0 rgba(5, 8, 9, 0.1), 0 5px 9px 1px rgba(5, 8, 9, 0.1) !important;
